bfb991740d12eb1681d3cca0612cc62008a9bb4d
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-04-07 23:49:16 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'dotenv-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails'
 # Use sqlite3 as the database for Active Record
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'capybara'
   gem 'shoulda-matchers'
+  gem 'pry'
 end
 gem 'webmock', group: :test
 
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,16 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    resp = Faraday.get('https://api.github.com/search/repositories') do |req|
+      req.params['client_id'] = ENV['clientID']
+      req.params['client_secret'] = ENV['clientSecret']
+      req.params['q'] = params['query']
+    end
+
+    @repos = JSON.parse(resp.body)['items']
+    render 'search'
   end
 end
